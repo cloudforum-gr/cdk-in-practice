da08cb5b8e0bca978f5822ed2287142f1e7a32b8
Caleb <calebjfrancisco@gmail.com>
2023-04-03 19:35:58 -0400

    Prepared test file for demo

---
+++ test/image-upload-stack.ts
@@ -0,0 +1,11 @@
+import * as cdk from "aws-cdk-lib";
+// import { Template } from "aws-cdk-lib/assertions";
+
+test("Image Upload Processing", () => {
+  const app = new cdk.App();
+  // const stack = ...
+  // const template = Template.fromStack(stack);
+  // template.hasResourceProperties("AWS::SQS::Queue", {
+  //   VisibilityTimeout: 120,
+  // });
+});
